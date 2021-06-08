94e91c0819de853917d7f099311a60c9f57fdb3b
Omar Ashour <omar.ashour22@gmail.com>
2021-06-08 14:50:15 -0700

    Enter submits new task

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, ButtonComponent, TextComponent } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, ButtonComponent, TextComponent, MarkdownPostProcessorContext} from 'obsidian';
 import * as fs from 'fs';
 //const applescript = require('applescript');
 import * as applescript from 'applescript'
@@ -55,6 +55,21 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
+		this.addCommand({
+			// Can use this to extract the database of tags and projects from OF
+			id: 'project-from-note',
+			name: 'Create project from current note',
+			 callback: () => {
+				let activeFile = this.app.workspace.getActiveFile();
+				if (activeFile){
+					console.log(activeFile.name);
+				}
+				else{
+					new Notice("No active file to create a project from.")
+				}
+			},
+		});
+
 		// Add the settings tab
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 	}
@@ -86,7 +101,7 @@ class InboxModal extends Modal {
 	onOpen() {
 		let {contentEl} = this;
 		// Add text box to hold the task name
-        let taskNameButton = new TextComponent(contentEl)
+        let taskNameField = new TextComponent(contentEl)
 				.setPlaceholder("Task Name")
 		// Button to add the tasks to OF
 		let addTaskButton = new ButtonComponent(contentEl)
@@ -95,53 +110,62 @@ class InboxModal extends Modal {
 			.setTooltip("Submit")
 			// On Click callback
 			.onClick(() => {
-				// Add quotes so it can be used in AppleScript
-				var taskName = "\"" + taskNameButton.getValue() + "\""
-				var taskNote = "\"Added by Obsidian on \""
-				const script = `set theDate to current date
-								set theTask to ${taskName}
-								set theNote to ${taskNote} & theDate
-				
-								tell application "OmniFocus"
-									tell front document
-										-- set theTag to first flattened tag where its name = "Office"
-										-- set theProject to first flattened project where its name = "Lin2019"
-										-- tell theProject to make new task with properties {name:theTask, note:theNote, primary tag:theTag}
-										-- make new inbox task with properties {name:theTask, note:theNote, primary tag:theTag}
-										make new inbox task with properties {name:theTask, note:theNote}
-									end tell
-								end tell`
-				// for debugging
-				console.log("This is the script for adding the task")
-				console.log(script)
-				// Execute apple script
-				applescript.execString(script, (err: any, rtn: any) => {
-							if (err) {
-								// Something went wrong!
-								console.log(err)
-								// What Reason?
-								new Notice("Error: Unable to create task for some reason")
-							}
-							else {
-								// Clear task button to add more tasks
-								taskNameButton.setValue("")
-								taskNameButton.inputEl.focus()
-							}
-							if (Array.isArray(rtn)) {
-								for (const value of rtn) {
-									console.log(value);
-								}
-							}
-							});
-			})
+				addTask(taskNameField);
+			});
     // Focus on the text field
-    taskNameButton.inputEl.focus()
+    taskNameField.inputEl.focus();
+	taskNameField.inputEl.addEventListener('keypress', function (keypressed) {
+		if (keypressed.key === 'Enter') {
+			addTask(taskNameField)
+		}
+	});
 	}
+
 	// What happens when you close the modal
 	onClose() {
 		let {contentEl} = this;
 		contentEl.empty();
 	}
+}
+function addTask(taskNameField: TextComponent){
+	// Add quotes so it can be used in AppleScript
+	var taskName = "\"" + taskNameField.getValue() + "\""
+	var taskNote = "\"Added by Obsidian on \""
+	const script = `set theDate to current date
+					set theTask to ${taskName}
+					set theNote to ${taskNote} & theDate
+	
+					tell application "OmniFocus"
+						tell front document
+							-- set theTag to first flattened tag where its name = "Office"
+							-- set theProject to first flattened project where its name = "Lin2019"
+							-- tell theProject to make new task with properties {name:theTask, note:theNote, primary tag:theTag}
+							-- make new inbox task with properties {name:theTask, note:theNote, primary tag:theTag}
+							make new inbox task with properties {name:theTask, note:theNote}
+						end tell
+					end tell`
+	// for debugging
+	console.log("This is the script for adding the task")
+	console.log(script)
+	// Execute apple script
+	applescript.execString(script, (err: any, rtn: any) => {
+				if (err) {
+					// Something went wrong!
+					console.log(err)
+					// What Reason?
+					new Notice("Error: Unable to create task for some reason")
+				}
+				else {
+					// Clear task button to add more tasks
+					taskNameField.setValue("")
+					taskNameField.inputEl.focus()
+				}
+				if (Array.isArray(rtn)) {
+					for (const value of rtn) {
+						console.log(value);
+					}
+				}
+				});
 }
 
 class SampleSettingTab extends PluginSettingTab {
